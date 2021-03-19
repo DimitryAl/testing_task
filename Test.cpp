16cd1237d2b3c63d03fee6b24cdb4db6bcb08213
Dmitry <mr.dima.alexeev@gmail.com>
2021-03-19 14:04:02 +0300

    Сделал проверку на X

--- Test.cpp
+++ Test.cpp
@@ -1,10 +1,10 @@
-п»ї// Test.cpp : Р­С‚РѕС‚ С„Р°Р№Р» СЃРѕРґРµСЂР¶РёС‚ С„СѓРЅРєС†РёСЋ "main". Р—РґРµСЃСЊ РЅР°С‡РёРЅР°РµС‚СЃСЏ Рё Р·Р°РєР°РЅС‡РёРІР°РµС‚СЃСЏ РІС‹РїРѕР»РЅРµРЅРёРµ РїСЂРѕРіСЂР°РјРјС‹.
+// Test.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
 #include <math.h>
 
-void output(int x) {
+void output(float x) {
     std::cout << x << "\t";
 }
 
@@ -19,7 +19,7 @@ void func(int* scrVertex, int* res) {
 
 void foo(float arr[], float maxWidth, float maxHeight) {
 
-    float dir[3] = { 0, 0, 0 };     //РЅР°РїСЂРІР»СЏСЋС‰РёР№ РІРµРєС‚РѕСЂ
+    float dir[3] = { 0, 0, 0 };     //напрвляющий вектор
     //float new_x, new_y, new_z;
     float triangle1[9];
     float triangle2[9];
@@ -31,7 +31,7 @@ void foo(float arr[], float maxWidth, float maxHeight) {
     float t;
 
 
-    // РїСЂРѕРІРµСЂРєР° РЅРµ РїСЂРµРІРѕСЃС…РѕРґРёС‚ Р»Рё С‚СЂРµСѓРіРѕР»СЊРЅРёРє РїРѕ С€РёСЂРёРЅРµ(РїСЂРѕРµРєС†РёСЏ РЅР° x )
+    // проверка не превосходит ли треугольник по ширине(проекция на x )
     if (abs(arr[0] - arr[3]) > maxWidth) {
         dir[0] = arr[3] - arr[0];
         dir[1] = arr[4] - arr[1];
@@ -41,9 +41,12 @@ void foo(float arr[], float maxWidth, float maxHeight) {
         t = (triangle1[0] - arr[0]) / (arr[3] - arr[0]);
         triangle1[1] = dir[1] * t + arr[1];
         triangle1[2] = dir[2] * t + arr[2];
+        triangle2[3] = triangle1[0];
+        triangle2[4] = triangle1[1];
+        triangle2[5] = triangle1[2];
 
         foo(triangle1, maxWidth, maxHeight);
-
+        foo(triangle2, maxWidth, maxHeight);
     }
     else if (abs(arr[0] - arr[6]) > maxWidth) {
         dir[0] = arr[6] - arr[0];
@@ -51,12 +54,9 @@ void foo(float arr[], float maxWidth, float maxHeight) {
         dir[2] = arr[8] - arr[2];
 
         triangle1[0] = arr[0] + maxWidth;
-        
-
         t = (triangle1[0] - arr[0]) / (arr[6] - arr[0]);
         triangle1[1] = dir[1] * t + arr[1];
         triangle1[2] = dir[2] * t + arr[2];
-        
         triangle2[6] = triangle1[0];
         triangle2[7] = triangle1[1];
         triangle2[8] = triangle1[2];
@@ -69,56 +69,43 @@ void foo(float arr[], float maxWidth, float maxHeight) {
         dir[1] = arr[7] - arr[4];
         dir[2] = arr[8] - arr[5];
 
-        triangle1[0] = arr[3] + maxWidth;
+        triangle1[6] = arr[3] + maxWidth;
         t = (triangle1[0] - arr[3]) / (arr[6] - arr[3]);
-        triangle1[1] = dir[1] * t + arr[4];
-        triangle1[2] = dir[2] * t + arr[5];
+        triangle1[7] = dir[1] * t + arr[4];
+        triangle1[8] = dir[2] * t + arr[5];
+        triangle2[3] = triangle1[6];
+        triangle2[4] = triangle1[7];
+        triangle2[5] = triangle1[8];
 
         foo(triangle1, maxWidth, maxHeight);
+        foo(triangle2, maxWidth, maxHeight);
+    }
+    else if (true) {
+
+    }
+    else if (true) {
+    }
+    else if (true) {
+
     }
     else {
         std::cout << "triangle is ok:\t";
         for (int i = 0; i < 9; i++) {
             output(arr[i]);
         }
-        std::cout << std::endl;
+        std::cout << std::endl << std::endl;
 
     }
 }
 
 
 int main()
-{   
-    /*int coords = 10;
-    int count = 5;
-    int n = 1;
-
-    int** scrVertex;
-    scrVertex = new int* [coords];
-    for (int i = 0; i < coords; i++) {
-        scrVertex[i] = new int [count];
-    }
-    int** res;
-    res = new int* [coords];
-    for (int i = 0; i < coords; i++) {
-        res[i] = new int[count];
-    }
-    for (int i = 0; i < coords; i++) {
-        for (int j = 0; j < count; j++) {
-            scrVertex[i][j] = n;
-            n++;
-        }
-    }
-    for (int i = 0; i < coords; i++) {
-        func(scrVertex[i], res[i]);
-    }
-    std::cout << res[2][4];*/
-
-    const int N = 9; 
+{
+    const int N = 9;
     float maxWidth = 10;
     float maxHeight = 10;
 
-    float vertex[N] = {50, 40, 10, 60, 50, 0, 70, 65, 30};
+    float vertex[N] = { 50, 40, 10, 60, 50, 0, 75, 65, 30 };
     for (int i = 0; i < N; i++) {
         output(vertex[i]);
     }
@@ -128,4 +115,3 @@ int main()
 
     return 0;
 }
-
